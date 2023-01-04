d9f5157e18993df34ef33a40fef26575092b27a5
SAR-dev <51741328+SAR-dev@users.noreply.github.com>
2023-01-04 16:54:51 +0900

    1 file modified, 1 added, 2 deleted

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,42 +1,14 @@
 import { useState, useLayoutEffect } from 'react';
 import { Page } from 'konsta/react';
-import WelcomeSvg from '@src/images/WelcomeSvg';
+import Leaves from '@src/images/leaves.jpg';
 
 export default function Signin() {
   return (
     <Page>
-      {/* <div className="h-screen w-full flex items-center justify-center overflow-hidden relative">
-        <div className="backdrop-blur-sm bg-black/20 h-screen flex items-center justify-center w-full">
-          <WelcomeSvg />
-        </div>
-        <div className="h-screen w-full flex items-center justify-center absolute top-0 left-0">
-          <button
-            type="button"
-            className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
-          >
-            <svg
-              className="w-4 h-4 mr-2 -ml-1"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="facebook-f"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-            >
-              <path
-                fill="currentColor"
-                d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
-              ></path>
-            </svg>
-            Sign in with Facebook
-          </button>
-        </div>
-      </div> */}
       <div
         className="h-screen flex items-center justify-center"
         style={{
-          backgroundImage: `url( 'https://www.sarahhearts.com/wp-content/uploads/2014/10/nov_2014_wallpaper_6plus.jpg' )`,
+          backgroundImage: `url( ${Leaves} )`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           backgroundPositionY: 'bottom',
@@ -44,7 +16,7 @@ export default function Signin() {
       >
         <button
           type="button"
-          className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+          className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 shadow-lg"
         >
           <svg
             className="w-4 h-4 mr-2 -ml-1"
