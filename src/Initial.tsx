709985fb58cb3d7f5183af36714ce3e7b8e93d61
SAR-dev <51741328+SAR-dev@users.noreply.github.com>
2023-01-04 13:25:53 +0900

    3 files modified

--- src/Initial.tsx
+++ src/Initial.tsx
@@ -20,71 +20,45 @@ export default function Initial({
   toggleDarkMode: () => void;
   toogleTheme: () => void;
 }) {
-  const [actionsOneOpened, setActionsOneOpened] = useState(false);
-  const [actionsTwoOpened, setActionsTwoOpened] = useState(false);
   return (
-    <Page style={{ backgroundImage: `url(${WelcomeImage})` }}>
-      <Navbar title="Action Sheet" />
-      <Block strong inset className="space-y-4 dark:bg-black">
-        <p>
-          Action Sheet is a slide-up pane for presenting the user with a set of
-          alternatives for how to proceed with a given task.
-        </p>
-      </Block>
-      <BlockTitle>Open Action Sheet</BlockTitle>
-      <Block strong inset className="flex space-x-4">
-        <Button onClick={() => setActionsOneOpened(true)}>One group</Button>
-        <Button onClick={() => setActionsTwoOpened(true)}>Two groups</Button>
-      </Block>
-
-      <Actions
-        opened={actionsOneOpened}
-        onBackdropClick={() => setActionsOneOpened(false)}
+    <Page>
+      <div
+        className="h-screen flex items-center justify-center"
+        style={{
+          backgroundImage: `url( 'https://i.ibb.co/BVwWKcQ/Vector-Wallpaper-056-300x585.jpg' )`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPositionY: 'bottom',
+        }}
       >
-        <ActionsGroup>
-          <ActionsLabel>Do something</ActionsLabel>
-          <ActionsButton
-            fontSizeIos="text-base"
-            onClick={() => setActionsOneOpened(false)}
-            bold
-          >
-            Button 1
-          </ActionsButton>
-          <ActionsButton
-            fontSizeIos="text-base"
-            onClick={() => setActionsOneOpened(false)}
-          >
-            Button 2
-          </ActionsButton>
-          <ActionsButton onClick={() => setActionsOneOpened(false)}>
-            Cancel
-          </ActionsButton>
-        </ActionsGroup>
-      </Actions>
-
-      <Actions
-        opened={actionsTwoOpened}
-        onBackdropClick={() => setActionsTwoOpened(false)}
-      >
-        <ActionsGroup>
-          <ActionsLabel>Do something</ActionsLabel>
-          <ActionsButton onClick={() => setActionsTwoOpened(false)} bold>
-            Button 1
-          </ActionsButton>
-          <ActionsButton onClick={() => setActionsTwoOpened(false)}>
-            Button 2
-          </ActionsButton>
-        </ActionsGroup>
-        <ActionsGroup>
-          <ActionsButton onClick={() => setActionsTwoOpened(false)}>
-            Cancel
-          </ActionsButton>
-        </ActionsGroup>
-      </Actions>
-      <Block strong inset className="flex space-x-4">
-        <Button onClick={() => toggleDarkMode()}>Toogle Mode</Button>
-        <Button onClick={() => toogleTheme()}>Toogle OS</Button>
-      </Block>
+        <Button className="w-52 normal-case bg-[#3b5998] hover:bg-[#3b5998]/90">
+          Sign in with Facebook
+        </Button>
+      </div>
     </Page>
   );
+}
+
+{
+  /* <button
+          type="button"
+          className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+        >
+          <svg
+            className="w-4 h-4 mr-2 -ml-1"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fab"
+            data-icon="facebook-f"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path
+              fill="currentColor"
+              d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
+            ></path>
+          </svg>
+          Sign in with Facebook
+        </button> */
 }
