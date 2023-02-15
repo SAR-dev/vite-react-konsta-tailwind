66a052ee7354df31ca2dc5df8a42748cf77678f7
SAR-dev <51741328+SAR-dev@users.noreply.github.com>
2023-02-16 01:13:47 +0900

    2 files modified

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -10,28 +10,30 @@ const TopNavbar = () => {
     <Navbar
       className="top-0 sticky h-14 flex items-center px-3"
       transparent={isTransparent}
-      right={
-        <div className="flex space-x-2 items-center">
-          <button className="btn btn-circle btn-sm btn-outline">
-            <MdOutlineNotificationsActive />
-          </button>
-          <button className="btn btn-circle btn-sm btn-outline">
-            <RiSettingsLine />
-          </button>
-        </div>
-      }
       left={
-        <div className="flex space-x-2 items-center">
-          {/* <img
-              src="https://i.postimg.cc/T2bMxdn9/image.png"
-              className="h-7 w-7 object-cover rounded-full"
-              alt=""
-            /> */}
-          <FaUserCircle size={30} />
+        <div className="flex space-x-3 items-center">
+          <div className="avatar">
+            <div className="rounded-full ring ring-gray-500 ring-offset-base-100 ring-offset-2">
+              <FaUserCircle size={30} />
+            </div>
+          </div>
           <div className="space-y-.5">
             <p className="font-semibold text-sm">Sayed Rafi</p>
             <p className="text-xs">Level 10</p>
           </div>
+        </div>
+      }
+      right={
+        <div className="flex space-x-2 items-center">
+          <button className="btn btn-sm btn-ghost btn-circle bg-gray-200/50 h-10 w-10">
+            <RiSettingsLine size={20} />
+          </button>
+          <button className="btn btn-sm btn-ghost btn-circle bg-gray-200/50 h-10 w-10">
+            <div className="indicator">
+              <MdOutlineNotificationsActive size={20} />
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
         </div>
       }
     />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,44 +18,6 @@ export default function Home() {
     <Page>
       <TopNavbar />
       <div className="relative">
-        <List strong inset>
-          {['Default', 'Medium', 'Large'].map((v) => (
-            <ListItem
-              key={v}
-              label
-              title={v}
-              after={
-                <Radio
-                  component="div"
-                  value={v}
-                  checked={size === v}
-                  onChange={() => setSize(v)}
-                />
-              }
-            />
-          ))}
-        </List>
-
-        <BlockTitle>Transparent</BlockTitle>
-
-        <BlockHeader>
-          When navbar is transparent, its title and background will become
-          visible on page scroll
-        </BlockHeader>
-        <List strong inset>
-          <ListItem
-            label
-            title="Transparent"
-            after={
-              <Toggle
-                component="div"
-                checked={isTransparent === true}
-                onChange={() => setIsTransparent(!isTransparent)}
-              />
-            }
-          />
-        </List>
-
         <Block strong inset className="space-y-4">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
@@ -68,66 +30,6 @@ export default function Home() {
             incidunt sunt fugiat tenetur odio, recusandae placeat rem veniam.
             Voluptates, repellendus odit, magni nesciunt, optio laborum
             asperiores repudiandae consectetur suscipit ab cupiditate eum.
-          </p>
-          <p>
-            Aliquam, iste accusamus deleniti temporibus exercitationem neque
-            perferendis optio, blanditiis quisquam molestias perspiciatis cumque
-            harum tenetur veniam. Dolorum fugit doloribus est, deserunt,
-            eligendi, quaerat quidem itaque tempore laborum non illum?
-          </p>
-          <p>
-            Rerum magni sunt quis veniam, dolor ratione saepe ducimus tempore
-            voluptatum porro quod commodi? Aperiam laudantium deleniti totam
-            dolorum qui accusantium iste saepe facere optio, soluta maxime
-            mollitia deserunt cumque.
-          </p>
-          <p>
-            Iusto tempore quis provident, saepe illum ex ipsum cupiditate
-            explicabo ratione unde facere nemo delectus harum, blanditiis eius
-            sit asperiores nam. Aut cupiditate est tempore officia, perspiciatis
-            esse asperiores repudiandae?
-          </p>
-          <p>
-            Consequuntur itaque harum eos vero, reiciendis dolorum iure non
-            earum molestias tenetur sint enim, maxime recusandae ad perferendis
-            repudiandae! Sit, quos exercitationem beatae numquam laborum nobis
-            natus. Obcaecati, ea inventore.
-          </p>
-          <p>
-            Fugit culpa labore sapiente excepturi reiciendis, nulla, nihil neque
-            ut veritatis quis quibusdam dolorum? Voluptatibus animi officia
-            perspiciatis doloremque cum voluptatem, quia ratione modi vero,
-            consequatur ipsum, praesentium quibusdam amet?
-          </p>
-          <p>
-            Laudantium nihil sint nam placeat, nemo rerum ipsam explicabo iusto
-            dolores molestiae expedita eos consequuntur ut architecto
-            consequatur soluta ad maiores voluptatem tenetur in velit. Minima
-            quia molestiae nobis voluptatibus.
-          </p>
-          <p>
-            Expedita soluta quia inventore et placeat id exercitationem quisquam
-            eligendi est eius sapiente quo, cum nesciunt mollitia, sit veniam
-            ducimus tempora culpa adipisci commodi in autem nihil voluptatem
-            corporis? Perspiciatis.
-          </p>
-          <p>
-            Molestias, est? Eligendi vero distinctio voluptatem cumque id
-            voluptatibus, officia minima repellendus sit illo tempora labore
-            provident? Eum tenetur consectetur quae, in facilis autem ipsam
-            doloribus voluptate vitae suscipit nobis.
-          </p>
-          <p>
-            Obcaecati optio iste hic, soluta minus ullam, perferendis pariatur
-            non possimus autem nostrum libero sapiente. Corporis quo cum iusto
-            exercitationem velit. Non beatae eveniet asperiores ipsa
-            consequuntur temporibus sapiente earum!
-          </p>
-          <p>
-            Temporibus, omnis. Excepturi dolorum expedita laudantium quasi quod
-            id adipisci, esse, nam atque in, incidunt ex ab distinctio
-            repellendus beatae voluptatem alias odit illum quis. Illo numquam
-            voluptatibus error voluptatum!
           </p>
         </Block>
       </div>
