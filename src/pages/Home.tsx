d76f24422f1831341b9cfe10dc163ddfd1d1c41a
SAR-dev <51741328+SAR-dev@users.noreply.github.com>
2023-02-16 01:31:47 +0900

    1 file modified

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,36 @@
-import { useState } from 'react';
-import {
-  Page,
-  Block,
-  BlockTitle,
-  BlockHeader,
-  List,
-  ListItem,
-  Radio,
-  Toggle,
-} from 'konsta/react';
+import { BsThreeDots } from 'react-icons/bs';
+import { Page, Block } from 'konsta/react';
 import TopNavbar from '@src/components/TopNavbar';
+import { FaUserCircle } from 'react-icons/fa';
+import { GiAmericanFootballHelmet } from 'react-icons/gi';
 
 export default function Home() {
-  const [size, setSize] = useState('Default');
-  const [isTransparent, setIsTransparent] = useState(false);
   return (
     <Page>
       <TopNavbar />
       <div className="relative">
         <Block strong inset className="space-y-4">
+          <div className="flex items-center w-full">
+            <div className="flex space-x-3 items-center">
+              <div className="avatar">
+                <div className="rounded-full ring ring-gray-500 ring-offset-base-100 ring-offset-2">
+                  <FaUserCircle size={30} />
+                </div>
+              </div>
+              <div className="space-y-.5">
+                <p className="font-semibold text-sm">Sayed Rafi</p>
+                <div className="flex items-center space-x-1">
+                  <GiAmericanFootballHelmet />
+                  <div className="text-xs">
+                    Chilling at Padma Bridge food court
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button className="ml-auto btn btn-xs btn-ghost">
+              <BsThreeDots size={20} />
+            </button>
+          </div>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
             tempore ratione unde accusantium distinctio nulla quia numquam earum
@@ -31,6 +43,50 @@ export default function Home() {
             Voluptates, repellendus odit, magni nesciunt, optio laborum
             asperiores repudiandae consectetur suscipit ab cupiditate eum.
           </p>
+          <div className="h-96 carousel carousel-vertical rounded-box">
+            <div className="carousel-item h-full">
+              <img
+                className="w-full object-cover"
+                src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+              />
+            </div>
+            <div className="carousel-item h-full">
+              <img
+                className="w-full object-cover"
+                src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+              />
+            </div>
+            <div className="carousel-item h-full">
+              <img
+                className="w-full object-cover"
+                src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+              />
+            </div>
+            <div className="carousel-item h-full">
+              <img
+                className="w-full object-cover"
+                src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+              />
+            </div>
+            <div className="carousel-item h-full">
+              <img
+                className="w-full object-cover"
+                src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+              />
+            </div>
+            <div className="carousel-item h-full">
+              <img
+                className="w-full object-cover"
+                src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+              />
+            </div>
+            <div className="carousel-item h-full">
+              <img
+                className="w-full object-cover"
+                src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              />
+            </div>
+          </div>
         </Block>
       </div>
     </Page>
